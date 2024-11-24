6aa79ab9abbc12ed58ebe59a849ff1f4533e7f76
fred <domotic70@gmail.com>
2024-11-24 17:16:27 +0100

    Work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.coxifred.pimpmyandroidpip"
-    compileSdk = 35
+    namespace = "com.coxifred.pimmyandroidpip"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.coxifred.pimpmyandroidpip"
+        applicationId = "com.coxifred.pimmyandroidpip"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
 
     }
 
@@ -32,44 +28,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-        viewBinding = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.tv.foundation)
-    implementation(libs.androidx.tv.material)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.core.splashscreen)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.leanback)
+    implementation(libs.glide)
+    implementation(libs.androidx.coordinatorlayout)
     implementation(libs.nanohttpd)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.cardview)
+    implementation(libs.gson)
+    implementation(libs.androidx.constraintlayout)
 }
